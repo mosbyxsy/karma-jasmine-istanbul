4e6016b13606926c1a5d33517aa41c5e8b56f13a
Mosby192 <545610830@qq.com>
2019-04-15 21:42:59 +0800

    enzyme

--- test/example.spec.js
+++ test/example.spec.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import Example from '../src/example';
 
-describe('example.jsx', function () {
-    it('使用React语法的测试', function () {
-        console.log(<Example text={name} />)
+const {shallow}=Enzyme;
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Enzyme的浅渲染测试套件', function () {
+    it('Example组件中按钮的名字为text的值', function () {
+        const name = '按钮名';
+        let app = shallow(<Example text={name} />);
+        expect(app.find('button').text()).toBe(name);
     })
 });
