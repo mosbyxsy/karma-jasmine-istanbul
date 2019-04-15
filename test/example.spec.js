f91609a6e297c140c37bb8f69df646b07189ec8f
Mosby192 <545610830@qq.com>
2019-04-15 23:06:28 +0800

    add chai

--- test/example.spec.js
+++ test/example.spec.js
@@ -3,6 +3,7 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Example from '../src/example';
 import expect from 'expect.js';
+import {assert} from 'chai'
 
 const {shallow}=Enzyme;
 
@@ -12,6 +13,6 @@ describe('Enzyme的浅渲染测试套件', function () {
     it('Example组件中按钮的名字为text的值', function () {
         const name = '按钮名';
         let app = shallow(<Example text={name} />);
-        expect(app.find('button').text()).to.equal(name);
+        assert.equal(app.find('button').text(),name);
     })
 });
